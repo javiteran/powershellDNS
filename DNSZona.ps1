ee7f261052e948119a6bc3258dba49f4b34298f3
Javier Terán González <javiteran@hotmail.com>
2020-10-05 18:47:01 +0200

    mejora visibilidad

--- DNSZona.ps1
+++ DNSZona.ps1
@@ -1,37 +1,37 @@
-#Creación y eliminación de una zona
+#Creacion y eliminacion de una zona
 #Este script debe ejecutarse en el servidor DNS donde quieres que se cree la zona.
-#Si no es así debería usarse el parámetro -computername
+#Si no es asa deberia usarse el parametro -computername  en TODAS LAS LINEAS
 
 $QueZona ='mizona.local'
 
 
-#Eliminación de la zona si existe. CUIDADO!!!!!
+#Eliminacion de la zona si existe. CUIDADO!!!!!
 Remove-DnsServerZone -Name $QueZona -Force 
 
-#Creación de una zona
+#Creacion de una zona
 Add-DnsServerPrimaryZone -Name $QueZona -ZoneFile "$QueZona.dns" 
-    #Eliminación del registro NS erroneo al crear la zona
-    Remove-DnsServerResourceRecord      -Name "@" -RRType Ns -ZoneName $QueZona -Force
+    #Eliminacion del registro NS erroneo al crear la zona
+    Remove-DnsServerResourceRecord      -RRType Ns                    -Name "@"  -ZoneName $QueZona -Force
 
     #Registros AAAA IPv6
-    Add-DnsServerResourceRecordAAAA     -IPv6Address 2001::108 -Name ns1 -ZoneName $QueZona
-    Add-DnsServerResourceRecordAAAA     -IPv6Address 2001::108 -Name www -ZoneName $QueZona
-    Add-DnsServerResourceRecordAAAA     -IPv6Address 2001::108 -Name mail -ZoneName $QueZona
+    Add-DnsServerResourceRecordAAAA     -IPv6Address 2001::108        -Name ns1  -ZoneName $QueZona
+    Add-DnsServerResourceRecordAAAA     -IPv6Address 2001::108        -Name www  -ZoneName $QueZona
+    Add-DnsServerResourceRecordAAAA     -IPv6Address 2001::108        -Name mail -ZoneName $QueZona
 
     #Registros A IPv4
-    Add-DnsServerResourceRecordA        -IPv4Address 172.20.140.254 -Name ns2 -ZoneName $QueZona
+    Add-DnsServerResourceRecordA        -IPv4Address 172.20.140.254   -Name ns2  -ZoneName $QueZona
 
     #Registros CNAME(Alias) o MX
-    Add-DnsServerResourceRecordCName    -HostNameAlias "www.$QueZona" -Name ftp -ZoneName $QueZona
-    Add-DnsServerResourceRecordMX       -MailExchange "mail.$QueZona" -Name "@" -Preference 10 -ZoneName $QueZona
+    Add-DnsServerResourceRecordCName    -Name ftp  -HostNameAlias "www.$QueZona" -ZoneName $QueZona
+    Add-DnsServerResourceRecordMX       -Name "@"  -MailExchange "mail.$QueZona" -ZoneName $QueZona -Preference 10 
     
-    #Creación de registros NS
+    #Creacion de registros NS
     Add-DnsServerResourceRecord         -Name "@" -NameServer "ns2.$QueZona" -NS -ZoneName $QueZona
     Add-DnsServerResourceRecord         -Name "@" -NameServer "ns1.$QueZona" -NS -ZoneName $QueZona
 
-    #Modificación del registro SOA
-    $OSOA = Get-DnsServerResourceRecord -ZoneName $QueZona -RRType SOA
-    $NSOA = Get-DnsServerResourceRecord -ZoneName $QueZona -RRType SOA
-    $NSOA.RecordData.PrimaryServer = "ns1.$QueZona."
-    $NSOA.RecordData.SerialNumber = "2020100308"
-    Set-DnsServerResourceRecord -NewInputObject $NSOA -OldInputObject $OSOA -ZoneName $QueZona
+    #Modificacion del registro SOA
+    $OSOA = Get-DnsServerResourceRecord  -RRType SOA                             -ZoneName $QueZona
+    $NSOA = Get-DnsServerResourceRecord  -RRType SOA                             -ZoneName $QueZona
+    $NSOA.RecordData.PrimaryServer  = "ns1.$QueZona."
+    $NSOA.RecordData.SerialNumber   = "2020100308"
+    Set-DnsServerResourceRecord     -OldInputObject $OSOA -NewInputObject $NSOA  -ZoneName $QueZona
